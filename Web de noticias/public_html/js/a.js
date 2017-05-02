1642abd090d56bd7ad29a409a396282756e8032e
josezero0 <josedavid.pc@outlook.com>
2017-05-03 00:38:46 +0200

    Actualización del proyecto

--- Web de noticias/public_html/js/a.js
+++ Web de noticias/public_html/js/a.js
@@ -1,16 +1,3 @@
 $(document).ready(function () {
-
-    var menu = $('.navbar');
-    var origOffsetY = menu.offset().top;
-    console.log(origOffsetY);
-
-//    $('#page').scroll(function () {
-//        console.log($('#page').scrollTop());
-//        if ($('#page').scrollTop() >= origOffsetY) {
-//            $('#container-nav').addClass('navbar-fixed-top');
-//        } else {
-//            $('.navbar').removeClass('navbar-fixed-top');
-//        }
-//    });
-
+    $('#nav-container').attr('data-offset-top',$('#nav-container').offset().top-15);
 });

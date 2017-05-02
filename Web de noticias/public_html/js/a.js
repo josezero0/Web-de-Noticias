acedb3452af76afc3d99f3fe7aa15616a99ad0dd
josezero0 <josedavid.pc@outlook.com>
2017-05-02 22:56:50 +0200

    Proyecto de netbeans agregado

---
+++ Web de noticias/public_html/js/a.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    var menu = $('.navbar');
+    var origOffsetY = menu.offset().top;
+    console.log(origOffsetY);
+
+//    $('#page').scroll(function () {
+//        console.log($('#page').scrollTop());
+//        if ($('#page').scrollTop() >= origOffsetY) {
+//            $('#container-nav').addClass('navbar-fixed-top');
+//        } else {
+//            $('.navbar').removeClass('navbar-fixed-top');
+//        }
+//    });
+
+});

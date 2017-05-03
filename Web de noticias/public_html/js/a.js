ecd490897096fe239e6c50a955eff7b862894e6b
josezero0 <josedavid.pc@outlook.com>
2017-05-03 22:38:38 +0200

    Actualización del esquema html
    Se han añadido las 3 primeras noticias precargadas en el html principal.
    Se ha añadido la publicidad para desktop/android
    Se ha deshabilitado temporalmente el .scroll() del javascript hasta que
    se haya completado la carga de noticias con el .scroll()

--- Web de noticias/public_html/js/a.js
+++ Web de noticias/public_html/js/a.js
@@ -1,12 +1,10 @@
 $(document).ready(function () {
     var offset = $('#nav-container').offset().top -15;
-    console.log(offset);
-    $('#pag').scroll(function(){
-        console.log($('#pag').scrollTop());
-        if($('#pag').scrollTop()>=offset){
-            $('#nav-container').addClass('affix');
-        }else{
-            $('#nav-container').removeClass('affix');
-        }
-    });
+//    $('#pag').scroll(function(){
+//        if($('#pag').scrollTop()>=offset){
+//            $('#nav-container').addClass('affix');
+//        }else{
+//            $('#nav-container').removeClass('affix');
+//        }
+//    });
 });

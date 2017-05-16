1b3210a1597bb7d8c5397da2374990b8c7d63af9
josezero0 <josedavid.pc@outlook.com>
2017-05-16 14:58:20 +0200

    2017.05.16 14:58

--- Web de noticias/public_html/js/a.js
+++ Web de noticias/public_html/js/a.js
@@ -1,10 +1,9 @@
 $(document).ready(function () {
-    var offset = $('#nav-container').offset().top -15;
-//    $('#pag').scroll(function(){
-//        if($('#pag').scrollTop()>=offset){
-//            $('#nav-container').addClass('affix');
-//        }else{
-//            $('#nav-container').removeClass('affix');
-//        }
-//    });
+    $('.body').scroll(function(){
+        if($('.body').scrollTop() > $('#header').outerHeight()){
+            $('.navbar').addClass('affix');
+        }else{
+            $('.navbar').removeClass('affix');
+        }
+    });
 });
---
+++ Web de noticias/public_html/js/news.js
@@ -0,0 +1,42 @@
+var json = ["data/1.json", "data/2.json"];
+var count = 0;
+
+$(document).ready(function () {
+    $('#load').click(function () {
+        loadJSON();
+    });
+    $('.body').scroll(function () {
+        if ($(this).scrollTop() + $(this).innerHeight() >= $(this)[0].scrollHeight) {
+            loadJSON();
+        }
+    });
+});
+
+function loadJSON() {
+    if (count >= json.length) {
+        //alert("No more news");
+    } else {
+        $.getJSON(json[count].toString(), function (jsonObject) {
+            createData(jsonObject);
+        });
+    }
+}
+
+function createData(jsonObject) {
+    $.each(jsonObject, function (i, noticia) {
+        var thumbnail = $('<div>', {class: "container-fluid thumbnail"});
+        var imgprev = $('<div>', {class: "imgprev"});
+        var img = $('<img>',{src: noticia.imgmid, alt: "otro"});
+        var caption = $('<div>', {class: "caption"});
+        var title = $('<h3>', {text: noticia.title});
+        title.append($('<br>'));
+        title.append($('<small>', {text: noticia.datetime}));
+        title.appendTo(caption);
+        caption.append($('<p>', {text: noticia.description}));
+        img.appendTo(imgprev);
+        imgprev.appendTo(thumbnail);
+        caption.appendTo(thumbnail);
+        thumbnail.appendTo($('#new-container'));
+    });
+    count++;
+}

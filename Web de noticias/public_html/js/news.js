f0b080f216a33bba8d345644596721f20fd0df5e
josezero0 <josedavid.pc@outlook.com>
2017-05-18 19:33:11 +0200

    2017.05.18 19:33

--- Web de noticias/public_html/js/news.js
+++ Web de noticias/public_html/js/news.js
@@ -24,15 +24,17 @@ function loadJSON() {
 
 function createData(jsonObject) {
     $.each(jsonObject, function (i, noticia) {
-        var thumbnail = $('<div>', {class: "container-fluid thumbnail"});
+        var thumbnail = $('<div>', {class: "container-fluid thumbnail text-justify"});
         var imgprev = $('<div>', {class: "imgprev"});
-        var img = $('<img>',{src: noticia.imgmid, alt: "otro"});
+        var img = $('<img>',{src: noticia.imgmid, alt: "otro", class: "img-responsive"});
         var caption = $('<div>', {class: "caption"});
         var title = $('<h3>', {text: noticia.title});
         title.append($('<br>'));
         title.append($('<small>', {text: noticia.datetime}));
         title.appendTo(caption);
-        caption.append($('<p>', {text: noticia.description}));
+        //caption.append($('<p>', {text: noticia.description}));
+        caption.append(noticia.description);
+        caption.append("<div class='text-center'><a class='btn btn-primary'>Leer más</a></div>");
         img.appendTo(imgprev);
         imgprev.appendTo(thumbnail);
         caption.appendTo(thumbnail);
